0b70fa624d96b3617512e527088261fcd4415205
Raparthishrikanth <raparthisrikanth29@gmail.com>
2026-06-09 16:48:49 +0530

    Fix IntegrityError duplicate key value in core_profile post_save signal

--- backend/django_app/core/models.py
+++ backend/django_app/core/models.py
@@ -21,14 +21,12 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.get_or_create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     if hasattr(instance, 'profile'):
         instance.profile.save()
-    else:
-        Profile.objects.create(user=instance)
 
 
 class Document(models.Model):
